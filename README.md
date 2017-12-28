d2b401676c0910ac8c86e54f7edeeec8093221f8
gustawdaniel <gustaw.daniel@gmail.com>
2017-12-28 20:36:34 +0100

    Basic docs added

---
+++ README.md
@@ -0,0 +1,7 @@
+# Install
+
+    meteor add gustawdaniel:reactive-set
+
+Further reading about Set object
+
+> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'gustawdaniel:reactive-set',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Reactive Set - object allowing store unique values of any type',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
