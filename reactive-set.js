de8ca02cdde45977a2e7722b9f0c92e136f171ff
gustawdaniel <gustaw.daniel@gmail.com>
2017-12-28 21:07:42 +0100

    Methods for get, set and has added.

--- reactive-set.js
+++ reactive-set.js
@@ -3,3 +3,34 @@
 // Variables exported by this module can be imported by other packages and
 // applications. See reactive-set-tests.js for an example of importing.
 export const name = 'reactive-set';
+
+ReactiveSet = function (value) {
+    check(value, Math.Optional(Set));
+
+    this._set = value || new Set();
+
+    this._dep = new Tracker.Dependency;
+};
+
+
+ReactiveSet.prototype.get = function() {
+    if (Tracker.active) {
+        this._dep.depend();
+    }
+    return this._set;
+};
+
+ReactiveSet.prototype.has = function(elem) {
+    if (Tracker.active) {
+        this._dep.depend();
+    }
+    return this._set.has(elem);
+};
+
+ReactiveSet.prototype.set = function(set) {
+    check(set, Math.Optional(Set));
+    if (!_.isEqual(set, this._set)) {
+        this._set = set;
+        this._dep.changed();
+    }
+};
